19ab5b092991c1604f5b429fd7c4d66e2cdb8e0f
Shamanth <shamanth081@gmail.com>
2025-08-18 17:13:04 +0530

    tesst

--- src/config.js
+++ src/config.js
@@ -1,7 +1,20 @@
-/* eslint-disable no-undef */
-export const API_BASE_URL = "http://localhost:5000/";
+// /* eslint-disable no-undef */
+// export const API_BASE_URL = "http://localhost:5000/";
+// export const BASE_URL = "/";
+// export const SOCKET_URL = "http://localhost:5000";
+// export const BASE_LINK = "/";
+// export const SOCKET_PATH = "/";
+// export const DEFAULT_IP_ADDRESS = "127.0.0.1";
+// export const AUTHOR_NAME = "nuvohosting";
+// export const JWT_SECRET = "secret!25!Nuvo"
+// export const LOGGER = process.env.NODE_ENV === 'development';
+// export const ROLES = {
+//   ADMIN: 'ADMIN',
+//   USER: 'USER'
+// }
+export const API_BASE_URL = "https://www.nuvohosting.org/api/";
 export const BASE_URL = "/";
-export const SOCKET_URL = "http://localhost:5000";
+export const SOCKET_URL = "https://www.nuvohosting.org";
 export const BASE_LINK = "/";
 export const SOCKET_PATH = "/";
 export const DEFAULT_IP_ADDRESS = "127.0.0.1";
